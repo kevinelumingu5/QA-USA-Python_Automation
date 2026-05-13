d78341c6b74eb28026210c9db02c189fc1a8e3a5
kevin <kevinelumingu5@gmail.com>
2026-05-13 07:24:30 -0500

    Project 8

--- pages.py
+++ pages.py
@@ -1,4 +1,5 @@
-from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 import time
@@ -22,7 +23,7 @@ class UrbanRoutesPage:
     CODE_INPUT_LOCATOR = (By.XPATH, '//input[@class="card-input" and @id="code"]')
     LINK_BUTTON_LOCATOR = (By.XPATH, '//button[text()="Link"]')
     X_CANCEL_CARD_BUTTON = (By.XPATH, '(//button[@class="close-button section-close"])[3]')
-    CARD_NUMBER_VALUE_LOCATOR = (By.XPATH, '//input[@id="number"]')
+    CARD_NUMBER_VALUE_LOCATOR = (By.XPATH, '(//div[text()="Card"])[2]')
 
     MESSAGE_DRIVER_INPUT_LOCATOR = (By.XPATH, '//*[@id="comment"]')
 
@@ -57,6 +58,7 @@ class UrbanRoutesPage:
         self.driver.find_element(*self.SUPPORTIVE_BUTTON_LOCATOR).click()
 
     def get_supportive(self):
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(self.CALL_TAXI_LOCATOR))
         return self.driver.find_element(*self.SUPPORTIVE_BUTTON_LOCATOR).text
 
     def enter_phone_number(self, phone_number):
@@ -81,14 +83,14 @@ class UrbanRoutesPage:
         self.driver.find_element(*self.X_CANCEL_CARD_BUTTON).click()
 
     def get_card_attribute(self):
-        return self.driver.find_element(*self.CARD_NUMBER_INPUT_LOCATOR).text
+        return self.driver.find_element(*self.CARD_NUMBER_VALUE_LOCATOR).text
 
 
     def enter_message(self, message):
         self.driver.find_element(*self.MESSAGE_DRIVER_INPUT_LOCATOR).send_keys(message)
 
     def get_message_attribute(self):
-        return self.driver.find_element(*self.MESSAGE_DRIVER_INPUT_LOCATOR).text
+        return self.driver.find_element(*self.MESSAGE_DRIVER_INPUT_LOCATOR).get_property('value')
 
 
     def activate_blankets_and_handkerchefs(self):
